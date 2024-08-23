d802a2e044a6d88f899a211e9d02a3d7e1302f65
Labani-png <labanimaitysmart@gmail.com>
2024-08-23 18:02:17 +0530

    learngeniee

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PageRouter from './PageRouter/PageRouter';
+
+function App() {
+  return (
+    <div>     
+
+     <PageRouter/>
+     
+    </div>
+  );
+}
+
+export default App;
